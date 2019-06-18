95d90173ac185edfd239f76251815e03b5a09e1c
Alok Choudhary <alok.c83@gmail.com>
2019-06-18 11:38:22 -0500

    Complete conversion to swift 5

---
+++ WeatherInfo/AppSettings/AppConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  AppConfiguration.swift
+//  WeatherInfo
+//
+//  Created by Alok Choudhary on 6/18/19.
+//  Copyright © 2019 Mt Aden LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppConfiguration {
+    struct ColorTheme {
+        static let globalTint = UIColor(hexString: "4cd964")
+    }
+}
